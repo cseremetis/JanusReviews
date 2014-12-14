ef2d4d9bd96d87e3861ef145cb47e59122dae080
github username <daniel.greenberg.15@gmail.com>
2014-12-14 10:07:10 -0500

    about

---
+++ views/About.rb
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--Created November 30, 2014 by Christian Seremetis and Daniel Greenberg-->
+<html>
+     <head>
+        <title>Review an Item</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md">
+                        <p class="navbar-text navbar-left">
+                            <a href="about" class="navbar-link">Home</a>
+                        </p>
+                    </div>
+                    <div class="col-md">
+                        <p class="navbar-text navbar-left">
+                            <a href="ExistingReviews" class="navbar-link">See Existing Reviews</a>
+                        </p>
+                    </div>
+                    <div class="col-md">
+                        <p class="navbar-text navbar-left">
+                            <a href="CreateReview" class="navbar-link">Review A New Item</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </nav>
